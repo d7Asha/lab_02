863bfeea5a37edcdab0f629317f563e386f3d98c
d7Asha <d1v07@yandex.ru>
2025-03-26 10:44:27 +0700

    Add user input feature to hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(){cout<<"Hello World"<<endl;return 0;}
+int main() 
+{
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello world from " << name << endl;
+	return 0;
+}
